4360e4eaba7c70a2139f7628d19c7606d357d132
DAKOTASTUDY <nix777@list.ru>
2023-10-18 13:16:12 -0600

    zaebalochanged

--- Nick_Ostrovsky_HW_4_1_2/Nick_Ostrovsky_HW_4_1_2.cpp
+++ Nick_Ostrovsky_HW_4_1_2/Nick_Ostrovsky_HW_4_1_2.cpp
@@ -12,8 +12,8 @@ int main()
     std::cout << array_length << std::endl;
     std::cout << "Please, enter your number:\n" << std::endl;
     std::cin >> user_int;
-   int a = numb(user_int, array_length,  in_array);
-   std::cout << "\n\t Maybe:" << a << std::endl;
+   int a = numb(user_int, array_length - 1,  in_array);
+   std::cout << "\n\t Maybe answer is:\t" << 9 - a << std::endl;
     return 0;
 }
 
@@ -21,34 +21,44 @@ int numb(int x, int max_index,  int array [])
 {
     
     int left_index = 0;
-    int right_index = max_index - 1;
-    std::cout << right_index << left_index << std::endl;
-    while (left_index < right_index)
+    int right_index = max_index ;
+   // std::cout << right_index << left_index << std::endl;
+    int middle ;//((left_index + right_index)) / 2;
+    while (left_index != right_index)
     {
    
-       
-        int middle = (left_index + right_index) / 2;
+        
+
+        middle = ((left_index + right_index)) / 2;
+        std::cout << right_index << left_index << middle << std::endl;
         if ((array[middle] == x))
         {
-            std::cout << "(array[middle] == x)\n" << right_index << left_index <<std::endl;
-            return max_index - middle;
+            while (((array[middle] == array[middle + 1]) && ((middle + 1)) < max_index))
+           { ++middle; }
+            std::cout << "\n\t middle" << middle <<std::endl;
+            return max_index  - middle;
             
         }
         else if ((x < array[middle]))
         {
-          right_index = middle - 1;
+          right_index = middle;
            
-           std::cout << "(x < array[middle])\n" <<  right_index << left_index <<std::endl;
+          
         }
-        else if(x > array[middle])
+        else if(x >= array[middle])
         {
            
             left_index = middle + 1 ;
-            std::cout << "x > array[middle]" << right_index << left_index << std::endl;
+           
         }
-         
+        
+      
+            std::cout << right_index << left_index << middle <<max_index<< std::endl;
+           if (left_index == right_index) {  return  right_index; }
     };
-
+   // std::cout << "\n\t middle" << middle << std::endl;
+  
+  
    
   //  std::cout <<"\n\t" << "items number: " << right_index << std::endl;
    
